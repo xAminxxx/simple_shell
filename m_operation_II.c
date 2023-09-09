a93cb3e57c1a03f8b114de286177b0a1bd7680c2
EMNA-ch <cheibi.emna90@gmail.com>
2023-09-09 08:31:49 -0700

    m_operation_II.c

---
+++ m_operation_II.c
@@ -0,0 +1,17 @@
+#include "simple_shell.h"
+
+/**
+ * ptr_free - frees the pointer
+ * @p: pointer's address
+ * Return: 1 if it is free, 0 if not.
+ */
+int ptr_free(void **p)
+{
+	if (*p && p)
+	{
+		free(*p);
+		*p = NULL;
+		return (1);
+	}
+	return (0);
+}
