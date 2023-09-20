de53fe025af42d60c01128ca29f68680b7e36acd
EMNA-ch <cheibi.emna90@gmail.com>
2023-09-20 05:28:04 -0700

    update files

---
+++ operation_1.c
@@ -0,0 +1,16 @@
+#include "simple_shell.h"
+
+/**
+ * check_delimit - checks if the character is delimeter
+ * @ch: the character to check
+ * @del: the delimeter
+ * Return: 1 if true or 0 if false
+ */
+int check_delimit( char ch, char *del)
+{
+	while (*del)
+		if (*del++ == ch)
+			return (1);
+	return (0);
+}
+
--- simple_shell.h
+++ simple_shell.h
@@ -7,11 +7,31 @@
 #include <string.h>
 #include <limits.h>
 
-/* string_functions.c */
+/* write and read buffers */
+#define W_BUFF_SIZE 1024
+#define BUFF_FLUSH -1
+#define R_BUFF_SIZE 1024
+
+/* str_f.c */
 int com_str(char *str1, char *str2);
 int length_str(char *str);
 char *concat_str(char *destination, char *source);
 int check_start(const char *substring, const char *hay);
+
+/* str_f_II.c */
+char *_dup(const char *word);
+char *_cpy(char *pasted, char *copied);
+void _view(char *txt);
+int _putchr(char a);
+
+/* str_f_III.c */
+char *_limconcat(char *txt1, char *txt2, int max);
+char *_limcpy(char *txt2, char *txt1, int max);
+char *_chrfind(char *txt, char chr);
+
+/*str_f_IV.c */
+char **strsplit(char *txt, char *del);
+char **strsplit2(char *txt, char del);
 
 /* m_operationI.c */
 void m_free(char **ss);
